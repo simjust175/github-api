704518de5b3c639092bad3c0d0b2b96416ecde44
simjust175 <bunim175@gmail.com>
2023-11-09 00:35:04 +0200

    added cofig + .env files

--- index.js
+++ index.js
@@ -1,167 +1,32 @@
 const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+const config = require("config");
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const port = config.get("port");
+const email = config.get("email.address")
+
+
+const USERS =[
+    {
+        "id": 1,
+        "name": "Jhon Doe",
+        "age": 45
     }
-);
+]
 
-// get user info
-app.get('/api/get-user', (req, res) => {
-    const token = req.headers.authorization;
-    console.log(token);
-    // get user info from github
-    axios.get('https://api.github.com/user', {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: token
-        }
-    })
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.send(error);
-        });
-    }
-);
+app.post("/user", (req, res)=>{
+    let user = req.body;
+    USERS.push(user)
+    res.status(201)
+    console.log(res.body);
+    res.send({"msg": "User successfully added", user})
+    res.send("Simcha / post")
+})
 
-//get users 
-app.get('/api/get-users', (req, res) => {
-    const token = req.headers.authorization;
-    console.log(token);
-    // get users from github
-    axios.get('https://api.github.com/users', {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: token
-        }
-    })
-        .then(response => {
-            // console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            // console.log(error);
-            res.send(error);
-        });
-    }
-);
-
-// get specific user info
-app.get('/api/get-user-info', (req, res) => {
-    const token = req.headers.authorization;
-    const user = req.query.user;
-    console.log(token);
-    // get user info from github
-    axios.get(`https://api.github.com/users/${user}`, {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: token
-        }
-    })
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.send(error);
-        });
-    }
-);
-
-// get user followers
-app.get('/api/get-followers', (req, res) => {
-    const token = req.headers.authorization;
-    console.log(token);
-    // get followers from github
-    axios.get('https://api.github.com/user/followers', {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: token
-        }
-    })
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.send(error);
-        });
-    }
-);
-
-
-// get user projects
-app.get('/api/get-projects', (req, res) => {
-    const token = req.headers.authorization;
-    console.log(token);
-    // get projects from github
-    axios.get('https://api.github.com/user/repos', {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: token
-        }
-    })
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.send(error);
-        });
-    }
-);
-
-app.get('/api/get-issues', (req, res) => {
-    const token = req.headers.authorization;
-    const project = req.query.project;
-    console.log(token);
-    // get projects from github
-    axios.get(`https://api.github.com/repos/${project}/issues`, {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: token
-        }
-    })
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.send(error);
-        });
-    }
-);
-
-app.get('/api/get-issues-comments', (req, res) => {
-    const token = req.headers.authorization;
-    const project = req.query.project;
-    const issue = req.query.issue;
-    console.log(token);
-    // get projects from github
-    axios.get(`https://api.github.com/repos/${project}/issues/${issue}/comments`, {
-        headers: {
-            Accept: 'application/vnd.github.v3+json',
-            Authorization: token
-        }
-    })
-        .then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.send(error);
-        });
+app.get('/', (req, res) => {  
+    res.send(email);
     }
 );
 
